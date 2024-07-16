7c4d587c7863c5f44a639d1e3ccb2732036d159e
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-07-16 16:20:51 +0900

    textField 추가

--- JBT_iOS/JBT_iOS/Sources/View/Auth/LoginViewController.swift
+++ JBT_iOS/JBT_iOS/Sources/View/Auth/LoginViewController.swift
@@ -5,7 +5,9 @@ import Then
 
 class LoginViewController: UIViewController {
     
-    private let label = JBTLoginTitleLabel(text: "로그인")
+    private let titleLabel = JBTLoginTitleLabel(text: "로그인")
+    private let idInputTF = JBTLoginTextField(type: .id)
+    private let pwInputTF = JBTLoginTextField(type: .pw)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,13 +17,27 @@ class LoginViewController: UIViewController {
     }
 
     func layout() {
-        [label].forEach { view.addSubview($0) }
+        [titleLabel, idInputTF, pwInputTF, ].forEach { view.addSubview($0) }
         
-        label.snp.makeConstraints {
+        titleLabel.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide).offset(80.0)
             $0.width.equalToSuperview().inset(24.0)
             $0.centerX.equalToSuperview()
             $0.height.equalTo(58.0)
+        }
+        
+        idInputTF.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(38.0)
+            $0.width.equalToSuperview().inset(24.0)
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(71.0)
+        }
+        
+        pwInputTF.snp.makeConstraints {
+            $0.top.equalTo(idInputTF.snp.bottom).offset(20.0)
+            $0.width.equalToSuperview().inset(24.0)
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(71.0)
         }
     }
 
